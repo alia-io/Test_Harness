ee88eaa250b2ec7c907f49f568fee2fda1d4a1cb
Ali Stith <alifastith@gmail.com>
2021-10-26 15:33:59 -0400

    TestRunner impl

--- TestChef/TestRunner.h
+++ TestChef/TestRunner.h
@@ -1,12 +1,20 @@
 #pragma once
+#include <exception>
+#include <string>
 #include "TestTimer.h"
 #include "TestExceptionHandler.h"
 
 class TestRunner {
 private:
-	TestTimer timer;
-	TestExceptionHandler exceptionHandler;
+	TestTimer timer {};
+	TestExceptionHandler exceptionHandler {};
+	bool (*testFunction)();
+	bool testResult = false;
+	std::string exceptionMsg = "";
 public:
-	bool runTest(void (*funcPtr)());
+	TestRunner(bool (*funcPtr)());
+	bool runTest();
+	bool getTestResult();
+	std::string getExceptionMsg();
+	double getTimeTaken();
 };
-
