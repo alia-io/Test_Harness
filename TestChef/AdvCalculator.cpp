952a2e996d9619993c85d73c6f9dece48dde51d9
jiawenzhen1 <jzhen02@syr.edu>
2021-11-06 21:00:44 -0400

    Added package for additional testing. Functions are similar to the basic calculator functions.

---
+++ TestChef/AdvCalculator.cpp
@@ -0,0 +1,39 @@
+#include "AdvCalculator.h"
+
+
+int AdvCalculator::exp(int operand1, int operand2) {
+	if (operand2 != 0){ //check if the power is non-zero
+		return (operand1 * AdvCalculator::exp(operand1, operand2 - 1)); //multiply the base with itself until power is zero
+	else
+		return 1; //power is zero and base^0=1
+	}
+}
+
+int AdvCalculator::modulo(int operand1, int operand2) {
+	return operand1 % operand2;
+}
+
+int AdvCalculator::factorial(int operand1) {
+	if (operand1 != 0){ //check if the power is non-zero
+		return (operand1 * AdvCalculator::factorial(operand1 - 1); //multiply the operand with operand-1 recursively
+	else
+			return 1; //factorial of zero = 1
+	}
+}
+
+int AdvCalculator::gcd(int operand1, int operand2) {
+	if (operand1 == 0){
+		return operand2;
+	}
+	if (operand2 == 0) {
+		return operand1;
+	}
+	
+	if (operand1 > operand2) {
+		return gcd(operand1 - operand2, operand2);
+	else
+		return gcd(operand1, operand2 - operand1);
+	}
+}
+
+
---
+++ TestChef/AdvCalculator.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+class AdvCalculator
+{
+
+public:
+	int exp(int operand1, int operand2);
+	int modulo(int operand1, int operand2);
+	int factorial(int operand1);
+	int gcd(int operand1, int operand2);
+};
+
