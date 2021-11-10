44cf1562c470e54f46128b6e7b64459d4a296c69
Ali Stith <alifastith@gmail.com>
2021-11-10 18:33:48 -0500

    added a couple more tests

--- TestChef/TestChef.cpp
+++ TestChef/TestChef.cpp
@@ -2,20 +2,20 @@
 //
 
 #include <iostream>
-#include "BasicCalculator.h"
-#include "AdvCalculator.h"
+#include <bitset>
+#include <vector>
+#include <functional>
 #include "TestLogger.h"
 #include "TestRunner.h"
 #include "TestHarness.h"
 #include "BasicCalculator.h"
-#include <vector>
-#include <functional>
 #include "BasicCalculatorTestDriver.cpp"
 #include "BasicCalculatorTestDriverFail.cpp"
 #include "BasicCalculatorTestDriverException.cpp"
 #include "AdvCalculatorTestDriver.cpp"
 #include "MemoryAllocatorTestDriverException1.cpp"
 #include "MemoryAllocatorTestDriverException2.cpp"
+#include "ContainerConversionsTestDriverFail.cpp"
 #include "ContainerConversionsTestDriverException.cpp"
 using namespace TestChef;
 using std::cout;
@@ -57,6 +57,12 @@ bool testMemoryAllocatorTestDriverExceptionScenario2() {
 	return allocator.TEST();
 }
 
+bool testContainerConversionsTestDriverFailScenario() {
+	// expect FAIL
+	ContainerConversionsTestDriverFail converter{};
+	return converter.TEST();
+}
+
 bool testContainerConversionsTestDriverExceptionScenario() {
 	// expect EXCEPTION
 	ContainerConversionsTestDriverException converter{};
@@ -65,8 +71,17 @@ bool testContainerConversionsTestDriverExceptionScenario() {
 
 bool testLengthError() {
 	// expect EXCEPTION
-	std::vector<int> myvector;
-	myvector.resize(myvector.max_size() + 1);
+	std::vector<int> vec;
+	vec.resize(vec.max_size() + 1);
+	return true;
+}
+
+bool testOverflowError() {
+	// expect EXCEPTION
+	std::bitset<100> bs;
+	bs[99] = 1;
+	bs[0] = 1;
+	unsigned long ul = bs.to_ulong();
 	return true;
 }
 
@@ -74,13 +89,15 @@ bool testLengthError() {
 int main() {
 	
 	TestHarness testHarness("def",LOGLEVEL::debug);
-	testHarness.addTests("testLengthError", * testLengthError);
 	testHarness.addTests("BasicCalculatorPassScenario", *testBasicCalculatorPassScenario);
 	testHarness.addTests("BasicCalculatorFailScenario", *testBasicCalculatorFailScenario);
 	testHarness.addTests("BasicCalculatorTestDriverExceptionScenario", *testBasicCalculatorTestDriverExceptionScenario);
 	testHarness.addTests("AdvCalculatorTestDriverPassScenario", *testAdvCalculatorTestDriverPassScenario);
 	testHarness.addTests("MemoryAllocatorTestDriverExceptionScenario1", *testMemoryAllocatorTestDriverExceptionScenario1);
 	testHarness.addTests("MemoryAllocatorTestDriverExceptionScenario2", *testMemoryAllocatorTestDriverExceptionScenario2);
+	testHarness.addTests("ContainerConversionsTestDriverFailScenario", *testContainerConversionsTestDriverFailScenario);
 	testHarness.addTests("ContainerConversionsTestDriverExceptionScenario", *testContainerConversionsTestDriverExceptionScenario);
+	testHarness.addTests("testLengthError", *testLengthError);
+	testHarness.addTests("testOverflowError", *testOverflowError);
 	testHarness.executor();
 }
