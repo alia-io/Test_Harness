cb45015b645c5bdb2c2cb9ae940fee8fc94b3454
jiawenzhen1 <jzhen02@syr.edu>
2021-11-29 20:48:43 -0500

    started the testmessagehandler for initiating the queues

--- TestChef/TestMessageHandler.cpp
+++ TestChef/TestMessageHandler.cpp
@@ -1,1 +1,21 @@
 #include "TestMessageHandler.h"
+
+void TestMessageHandler::initiateQueues() {
+	queue<int> readyMessageQueue;
+	queue<int> testRequestMessageQueue;
+	queue<int> testRequestListMessageQueue;
+
+	startQueue(readyMessageQueue);
+	startQueue(testRequestMessageQueue);
+	startQueue(testRequestListMessageQueue);
+}
+
+void TestMessageHandler::startQueue(std::queue<int> q)
+{
+	
+}
+
+
+void TestMessageHandler::addMessageForParent()
+{
+}
--- TestChef/TestMessageHandler.h
+++ TestChef/TestMessageHandler.h
@@ -1,5 +1,22 @@
 #pragma once
+///////////////////////////////////////////////////////////////
+//TestMessageHandler.h - Class that implements the blocking  //
+//queues											         //
+//Santhosh Srinivasan, Jiawen Zhen, Alifa Stith              //
+//														     //
+///////////////////////////////////////////////////////////////
 
+#include <iostream>
+#include <stdlib.h>
+#include <queue>
+
+using namespace std;
 class TestMessageHandler {
+public:
+	void addMessageForParent();
+	void initiateQueues();
+	void startQueue(std::queue<int> q);
+
+private:
 
 };
