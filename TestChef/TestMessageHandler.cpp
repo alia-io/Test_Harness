4e13c073fd8110c1816a818836ec96ea35818a2f
Ali Stith <alifastith@gmail.com>
2021-12-04 11:52:01 -0500

    fixed mistake

--- TestChef/TestMessageHandler.cpp
+++ TestChef/TestMessageHandler.cpp
@@ -1,7 +1,5 @@
 #include "TestMessageHandler.h"
 
-#include "TestMessageHandler.h"
-
 // for enqueueTestResult: the message body should be formatted in JSON as follows:
 // { "result": "pass/fail/exception", "message": "[msg_string]" }
 // [msg_string] is the original "body" that is passed as an argument
