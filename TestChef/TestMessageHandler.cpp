b1164477969963f8f1db8b4e27b2872d0c4f31f5
jiawenzhen1 <jzhen02@syr.edu>
2021-12-04 11:21:14 -0500

    new updates to message handler class

--- TestChef/TestMessageHandler.cpp
+++ TestChef/TestMessageHandler.cpp
@@ -1,37 +1,9 @@
 #include "TestMessageHandler.h"
 
-
-void TestMessageHandler::addMessagetoQueue()
-{	
-	auto type = getMessageType();		//obtain the message type
-	std::string msg = getMessage();		//obtain the message contents
-
-	if (type == MESSAGE_TYPE::request) {		//The message is a request type
-		testRequestMessageQueue.push(msg);		//Add the message to the back of the testRequestMessageQueue
-	}
-
-	if (type == MESSAGE_TYPE::request_list) {
-		testRequestListMessageQueue.push(msg);			//add the DLL request to the testRequestListMessageQueue
-	}
-}
-
-void TestMessageHandler::releaseMessageFromQueue()
+void TestMessageHandler::enqueueTestRequest(TestItem item)
 {
-	auto type = getMessageType();		//obtain the message type
-	std::string msg = getMessage();		//obtain the message contents
-
-	if (type == MESSAGE_TYPE::response) {	//The message type is a response
-		testRequestMessageQueue.pop();		//Remove the message from queue now that response has been recieved
-	}
-
-	if (type == MESSAGE_TYPE::response_list) {			//message type is a response list
-		cout << testRequestListMessageQueue.front();	//Remove the DLL list message from queue now that response has been recieved
-	}
 }
 
-int TestMessageHandler::queueSize(queue<string> qu){ return qu.size();}
-
-string TestMessageHandler::getFrontQueue(queue<string> qu) { return qu.front();}
-
-string TestMessageHandler::getBackQueue(queue<string> qu) { return qu.back();}
-
+void TestMessageHandler::enqueueTestResult(std::thread::id parentThreadId, TEST_RESULT result, std::string body)
+{
+}
--- TestChef/TestMessageHandler.h
+++ TestChef/TestMessageHandler.h
@@ -16,17 +16,10 @@ using namespace std;
 
 class TestMessageHandler {
 private:
-	//queue<string> readyMessageQueue;
-	//queue<string> testRequestMessageQueue;
-	//queue<string> testRequestListMessageQueue;
 	TestBlockingQueue<TestItem> testRequests{};
 	TestBlockingQueue<TestMessage> testResults{};
 public:
-	//void addMessagetoQueue();					//Add a msg to queue based on type
-	//void releaseMessageFromQueue();				//Release a msg from queue based on type
-	//int queueSize(queue<string> qu);			//Obtain size of queue
-	//string getFrontQueue(queue<string> qu);	//Obtain from front of message queue
-	//string getBackQueue(queue<string> qu);
+
 	void enqueueTestRequest(TestItem item);
 	void enqueueTestResult(std::thread::id parentThreadId, TEST_RESULT result, std::string body);
 };
