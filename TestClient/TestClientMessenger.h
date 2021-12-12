d3cf249ce1da0f8519abedf156d6a3cdfa7a08e8
jiawenzhen1 <jzhen02@syr.edu>
2021-12-12 16:11:27 -0500

    Made edits to the TestClientMessenger.cpp and .h classes

--- TestClient/TestClientMessenger.h
+++ TestClient/TestClientMessenger.h
@@ -1,1 +1,13 @@
 #pragma once
+#include <string>
+#include <vector>
+class TestClientMessenger {
+private:
+	int numDLLs;						//indicates the number of DLLs
+	std::vector<std::string> dllNames;
+	std::string JSONClientMessage;
+
+public:
+	std::string createJSONClientMessage();
+	int getNumDLLs();
+};
